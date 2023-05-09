1b9c0d948fcf87a019699ddf93233254891f198a
Dwayne Schultz <myshkin5@users.noreply.github.com>
2023-05-09 09:15:27 -0600

    Switch to forked deephash (#5)
    * Switch to forked deephash
    
    * Appease linter
    
    * Troubleshoot codecov issue, update codecov orb

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module moqueries.org/runtime
 
 go 1.15
 
-require github.com/davegardnerisme/deephash v0.0.0-20210406090112-6d072427d830
+require moqueries.org/deephash v0.0.1
--- hash/hash.go
+++ hash/hash.go
@@ -2,24 +2,12 @@
 // used as a key in hash maps
 package hash
 
-import (
-	"encoding/binary"
-
-	"github.com/davegardnerisme/deephash"
-)
-
-const hashBytes = 8
+import "moqueries.org/deephash"
 
 // Hash stores the hash of a source object
 type Hash uint64
 
 // DeepHash walks the src parameter and produces a hash
 func DeepHash(src interface{}) Hash {
-	b := deephash.Hash(src)
-	if len(b) < hashBytes {
-		newB := make([]byte, hashBytes)
-		copy(newB, b)
-		b = newB
-	}
-	return Hash(binary.LittleEndian.Uint64(b))
+	return Hash(deephash.Hash(src))
 }
